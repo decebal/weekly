358f83161aeb98682ee79344f113258420e7a41c
mktcode <kontakt@markus-kottlaender.de>
2017-12-30 15:21:32 +0100

    added config file

---
+++ config.dist.js
@@ -0,0 +1,12 @@
+var config ={
+  smtp: {
+    host: 'host.tld',
+    user: 'user',
+    pass: 'pass'
+  },
+  mail: {
+    from: 'Utopian.io <weekly@utopian.io>',
+    testRecipient: 'test@example.org'
+  }
+}
+module.exports = config
--- src/weekly.js
+++ src/weekly.js
@@ -1,40 +1,31 @@
 'use strict';
 const nodemailer = require('nodemailer');
+const config = require('../config.js');
 
-// Generate test SMTP service account from ethereal.email
-// Only needed if you don't have a real mail account for testing
-nodemailer.createTestAccount((err, account) => {
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    host: config.smtp.host,
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: config.smtp.user, // generated ethereal user
+        pass: config.smtp.pass  // generated ethereal password
+    }
+});
 
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: account.user, // generated ethereal user
-            pass: account.pass  // generated ethereal password
-        }
-    });
+// setup email data with unicode symbols
+let mailOptions = {
+    from: config.mail.from, // sender address
+    to: config.mail.testRecipient, // list of receivers
+    subject: 'Utopian Weekly', // Subject line
+    text: 'Utopian Weekly', // plain text body
+    html: '<h1>Utopian Weekly</h1>' // html body
+};
 
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
-        to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
-        subject: 'Hello ✔', // Subject line
-        text: 'Hello world?', // plain text body
-        html: '<b>Hello world?</b>' // html body
-    };
-
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-        // Preview only available when sending through an Ethereal account
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-    });
+// send mail with defined transport object
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
 });
