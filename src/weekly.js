3b7683502c9fe38503b9bfa529c46a4706825a49
mktcode <kontakt@markus-kottlaender.de>
2017-12-30 20:59:25 +0100

    removed .js suffix

--- src/weekly.js
+++ src/weekly.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const request = require('request');
 const fs = require('fs');
 const steem = require('steem');
-const config = require('../config.js');
+const config = require('../config');
 
 // date settings
 let startDate = new Date();
