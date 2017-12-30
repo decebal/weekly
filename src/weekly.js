0e7fb271270d0b5dad290aa691860601a7f44b0d
Nico Wehmöller <nico.wehmoeller@eripini.win>
2017-12-30 22:49:44 +0100

    added function to get moderators and sponsors

--- src/weekly.js
+++ src/weekly.js
@@ -19,114 +19,145 @@ const contributionsUrl = config.apiUrl + '/api/posts/top?limit=3&start_date=' + 
 
 // get utopian projects/contributions
 let data = {
-  projects: null,
-  newcomers: null,
-  contributions: null
+    projects: null,
+    newcomers: null,
+    contributions: null
 };
 
 let getData = new Promise((yes, no) => {
-  request(projectsUrl, (err, response, body) => {
-    if (err) no(err);
-    yes(body);
-  });
+    request(projectsUrl, (err, response, body) => {
+        if (err) no(err);
+        yes(body);
+    });
 });
 
 getData.then((projects) => {
-  data.projects = projects
-  request(newcomersUrl, (err, response, body) => {
-    if (err) console.log(err);
-    return body;
-  });
+    data.projects = projects
+    request(newcomersUrl, (err, response, body) => {
+        if (err) console.log(err);
+        return body;
+    });
 });
 
 getData.then((newcomers) => {
-  data.newcomers = newcomers
-  request(contributionsUrl, (err, response, body) => {
-    if (err) console.log(err);
-    return body;
-  });
+    data.newcomers = newcomers
+    request(contributionsUrl, (err, response, body) => {
+        if (err) console.log(err);
+        return body;
+    });
 });
 
 getData.then((contributions) => {
-  data.contributions = contributions
+    data.contributions = contributions
 
-  // create reusable transporter object using the default SMTP transport
-  let mailer = nodemailer.createTransport({
-    host: config.smtp.host,
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: config.smtp.user,
-      pass: config.smtp.pass
-    }
-  });
-
-  // read html template
-  fs.readFile('./src/template.html', 'utf-8', function read(err, rawTemplate) {
-    if (err) {
-      throw err;
-    }
-
-    const template = generateAndSaveTemplate('html', rawTemplate, '1/2018', projects, contributions);
-
-    // mail setup
-    const mailOptions = {
-      from: config.mail.from,
-      to: config.mail.testRecipient,
-      subject: 'Utopian Weekly',
-      text: 'Utopian Weekly',
-      html: template
-    };
-
-    // send mail
-    mailer.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          return console.log(error);
-      }
-      console.log('Message sent: %s', info.messageId);
+    // create reusable transporter object using the default SMTP transport
+    let mailer = nodemailer.createTransport({
+        host: config.smtp.host,
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: config.smtp.user,
+            pass: config.smtp.pass
+        }
     });
-  });
 
-  // read markdown template
-  fs.readFile('./src/template.md', 'utf-8', function read(err, rawTemplate) {
-    if (err) {
-        throw err;
-    }
+    // read html template
+    fs.readFile('./src/template.html', 'utf-8', function read(err, rawTemplate) {
+        if (err) {
+            throw err;
+        }
 
-    const template = generateAndSaveTemplate('md', rawTemplate, '1/2018', projects, contributions);
+        const template = generateAndSaveTemplate('html', rawTemplate, '1/2018', projects, contributions);
 
-    // post on steemit.com
-    var permlink = new Date().toISOString().replace(/[^a-zA-Z0-9]+/g, '').toLowerCase();
+        // mail setup
+        const mailOptions = {
+            from: config.mail.from,
+            to: config.mail.testRecipient,
+            subject: 'Utopian Weekly',
+            text: 'Utopian Weekly',
+            html: template
+        };
 
-    steem.broadcast.comment(
-      config.wif,
-      'mkt', // Parent Author
-      'steemline-beta-typescript-and-steemconnect-integration-maintainer-wanted-50-steem', // Parent Permlink
-      'guest123', // Author
-      permlink, // Permlink
-      'Utopian Weekly', // Title
-      template, // Body,
-      {tags: ['test'], app: 'steemjs/utopianweekly'}, // Json Metadata
-      (err, result) => {
-          console.log(err, result);
-      }
-    );
-  });
+        // send mail
+        mailer.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+        });
+    });
+
+    // read markdown template
+    fs.readFile('./src/template.md', 'utf-8', function read(err, rawTemplate) {
+        if (err) {
+            throw err;
+        }
+
+        const template = generateAndSaveTemplate('md', rawTemplate, '1/2018', projects, contributions);
+
+        // post on steemit.com
+        var permlink = new Date().toISOString().replace(/[^a-zA-Z0-9]+/g, '').toLowerCase();
+
+        steem.broadcast.comment(
+            config.wif,
+            'mkt', // Parent Author
+            'steemline-beta-typescript-and-steemconnect-integration-maintainer-wanted-50-steem', // Parent Permlink
+            'guest123', // Author
+            permlink, // Permlink
+            'Utopian Weekly', // Title
+            template, // Body,
+            {tags: ['test'], app: 'steemjs/utopianweekly'}, // Json Metadata
+            (err, result) => {
+                console.log(err, result);
+            }
+        );
+    });
 });
 
 // fill template with data and save static file
 function generateAndSaveTemplate(ext, content, number, projects, contributions) {
-  // replace placeholders
-  content = content.replace('${NUMBERING}', number);
+    // replace placeholders
+    content = content.replace('${NUMBERING}', number);
 
-  // save file
-  let filename = './static/archive/utopian-weekly-' + startDate + '-' + endDate + '.' + ext;
-  fs.writeFile(filename, content, function(err) {
-    if (err) {
-        return console.log(err);
-    }
-    console.log('Template saved! (' + filename + ')');
-  });
+    // save file
+    let filename = './static/archive/utopian-weekly-' + startDate + '-' + endDate + '.' + ext;
+    fs.writeFile(filename, content, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('Template saved! (' + filename + ')');
+    });
 
-  return content;
+    return content;
+}
+
+
+function getModerators() {
+    return new Promise((yes, no) => {
+        request(config.apiUrl + '/api/moderators', (err, res, body) => {
+            if (err) no(err);
+            yes(JSON.parse(body));
+        })
+    }).then((response) => {
+        let mods = [];
+        response.results.forEach((mod) => {
+            mods.push(mod.account);
+        });
+        return mods;
+    })
+}
+
+function getSponsors() {
+    return new Promise((yes, no) => {
+        request(config.apiUrl + '/api/sponsors', (err, res, body) => {
+            if (err) no(err);
+            yes(JSON.parse(body));
+        })
+    }).then((response) => {
+        let sponsors = [];
+        response.results.forEach((sponsor) => {
+            sponsors.push(sponsor.account);
+        });
+        return sponsors;
+    })
 }
