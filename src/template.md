12cc2d81c52e2117a532229b4f373f4c3b6b362d
mktcode <kontakt@markus-kottlaender.de>
2018-01-03 03:22:35 +0100

    updated template, fixed promises

--- src/template.md
+++ src/template.md
@@ -15,7 +15,7 @@
 ### [{{ data.projects[0].github.name }}]({{ data.projects[0].project_url }}) ({{ data.projects[0].count }} Contributions)
 {{ data.projects[0].github.description }}
 
-[![{{ data.projects[0].github.name }}](${PROJECT_1_IMAGE})]({{ data.projects[0].project_url }})
+[![{{ data.projects[0].github.name }}](https://res.cloudinary.com/hpiynhbhq/image/upload/v1510150908/xx4shp2yiekby5d6sify.png)]({{ data.projects[0].project_url }})
 
 </td></tr></table>
 
@@ -24,14 +24,14 @@
 ### [{{ data.projects[1].github.name }}]({{ data.projects[1].project_url }}) ({{ data.projects[1].count }} Contributions)
 {{ data.projects[1].github.description }}
 
-[![{{ data.projects[1].github.name }}](${PROJECT_1_IMAGE})]({{ data.projects[1].project_url }})
+[![{{ data.projects[1].github.name }}](https://res.cloudinary.com/hpiynhbhq/image/upload/v1510150908/xx4shp2yiekby5d6sify.png)]({{ data.projects[1].project_url }})
 
 </td><td>
 
 ### [{{ data.projects[2].github.name }}]({{ data.projects[2].project_url }}) ({{ data.projects[2].count }} Contributions)
 {{ data.projects[2].github.description }}
 
-[![{{ data.projects[2].github.name }}](${PROJECT_1_IMAGE})]({{ data.projects[2].project_url }})
+[![{{ data.projects[2].github.name }}](https://res.cloudinary.com/hpiynhbhq/image/upload/v1510150908/xx4shp2yiekby5d6sify.png)]({{ data.projects[2].project_url }})
 
 </td></tr></table>
 
@@ -43,14 +43,14 @@
 ### [{{ data.newcomers[0].github.name }}]({{ data.newcomers[0].project_url }}) ({{ data.newcomers[0].count }} Contributions)
 {{ data.newcomers[0].github.description }}
 
-[![{{ data.newcomers[0].github.name }}](${PROJECT_1_IMAGE})]({{ data.newcomers[0].project_url }})
+[![{{ data.newcomers[0].github.name }}](https://res.cloudinary.com/hpiynhbhq/image/upload/v1510150908/xx4shp2yiekby5d6sify.png)]({{ data.newcomers[0].project_url }})
 
 </td><td>
 
 ### [{{ data.newcomers[1].github.name }}]({{ data.newcomers[1].project_url }}) ({{ data.newcomers[1].count }} Contributions)
 {{ data.newcomers[1].github.description }}
 
-[![{{ data.newcomers[1].github.name }}](${PROJECT_1_IMAGE})]({{ data.newcomers[1].project_url }})
+[![{{ data.newcomers[1].github.name }}](https://res.cloudinary.com/hpiynhbhq/image/upload/v1510150908/xx4shp2yiekby5d6sify.png)]({{ data.newcomers[1].project_url }})
 
 </td></tr></table>
 
@@ -59,14 +59,14 @@
 ### [{{ data.newcomers[2].github.name }}]({{ data.newcomers[2].project_url }}) ({{ data.newcomers[2].count }} Contributions)
 {{ data.newcomers[2].github.description }}
 
-[![{{ data.newcomers[2].github.name }}](${PROJECT_1_IMAGE})]({{ data.newcomers[2].project_url }})
+[![{{ data.newcomers[2].github.name }}](https://res.cloudinary.com/hpiynhbhq/image/upload/v1510150908/xx4shp2yiekby5d6sify.png)]({{ data.newcomers[2].project_url }})
 
 </td><td>
 
 ### [{{ data.newcomers[3].github.name }}]({{ data.newcomers[3].project_url }}) ({{ data.newcomers[3].count }} Contributions)
 {{ data.newcomers[3].github.description }}
 
-[![{{ data.newcomers[3].github.name }}](${PROJECT_1_IMAGE})]({{ data.newcomers[3].project_url }})
+[![{{ data.newcomers[3].github.name }}](https://res.cloudinary.com/hpiynhbhq/image/upload/v1510150908/xx4shp2yiekby5d6sify.png)]({{ data.newcomers[3].project_url }})
 
 
 </td></tr></table>
@@ -74,48 +74,22 @@
 # <center>Top 10 Contributions</center>
 #
 
-1. [${CONTRIBUTION_1_TITLE}](${CONTRIBUTION_1_URL}) (${CONTRIBUTION_1_REWARD} SBD)
-by [${CONTRIBUTION_1_USER}](https://utopian.io/@${CONTRIBUTION_1_USER})
+{% for contribution in data.contributions %}
+[{{ contribution.title }}]({{ contribution.utopian_url }})
+by [{{ contribution.author }}](https://utopian.io/@{{ contribution.author }}), {{ contribution.active_votes | length }} Upvotes, {{ contribution.rewards | number_format(2) }} SBD
 
-2. [${CONTRIBUTION_2_TITLE}](${CONTRIBUTION_2_URL}) (${CONTRIBUTION_2_REWARD} SBD)
-by [${CONTRIBUTION_2_USER}](https://utopian.io/@${CONTRIBUTION_2_USER})
-
-3. [${CONTRIBUTION_3_TITLE}](${CONTRIBUTION_3_URL}) (${CONTRIBUTION_3_REWARD} SBD)
-by [${CONTRIBUTION_3_USER}](https://utopian.io/@${CONTRIBUTION_3_USER})
-
-4. [${CONTRIBUTION_4_TITLE}](${CONTRIBUTION_4_URL}) (${CONTRIBUTION_4_REWARD} SBD)
-by [${CONTRIBUTION_4_USER}](https://utopian.io/@${CONTRIBUTION_4_USER})
-
-5. [${CONTRIBUTION_5_TITLE}](${CONTRIBUTION_5_URL}) (${CONTRIBUTION_5_REWARD} SBD)
-by [${CONTRIBUTION_5_USER}](https://utopian.io/@${CONTRIBUTION_5_USER})
-
-6. [${CONTRIBUTION_6_TITLE}](${CONTRIBUTION_6_URL}) (${CONTRIBUTION_6_REWARD} SBD)
-by [${CONTRIBUTION_6_USER}](https://utopian.io/@${CONTRIBUTION_6_USER})
-
-7. [${CONTRIBUTION_7_TITLE}](${CONTRIBUTION_7_URL}) (${CONTRIBUTION_7_REWARD} SBD)
-by [${CONTRIBUTION_7_USER}](https://utopian.io/@${CONTRIBUTION_7_USER})
-
-8. [${CONTRIBUTION_8_TITLE}](${CONTRIBUTION_8_URL}) (${CONTRIBUTION_8_REWARD} SBD)
-by [${CONTRIBUTION_8_USER}](https://utopian.io/@${CONTRIBUTION_8_USER})
-
-9. [${CONTRIBUTION_9_TITLE}](${CONTRIBUTION_9_URL}) (${CONTRIBUTION_9_REWARD} SBD)
-by [${CONTRIBUTION_9_USER}](https://utopian.io/@${CONTRIBUTION_9_USER})
-
-10. [${CONTRIBUTION_10_TITLE}](${CONTRIBUTION_10_URL}) (${CONTRIBUTION_10_REWARD} SBD)
-by [${CONTRIBUTION_10_USER}](https://utopian.io/@${CONTRIBUTION_10_USER})
+{% endfor %}
 
 <hr />
 
-### A Special Thanks to The Sponsors
-
-{% for moderator in data.moderators %}
-@{{ moderator }}
-{% if not loop.last %}, {% endif %}
-{% endfor %}
-
-#### A Special Thanks to The Moderators
+#### A Special Thanks to The Sponsors
 
 {% for sponsor in data.sponsors %}
-@{{ sponsor }}
-{% if not loop.last %}, {% endif %}
+@{{ sponsor.account }}{% if not loop.last %}, {% endif %}
+{% endfor %}
+
+### A Special Thanks to The Moderators
+
+{% for moderator in data.moderators %}
+@{{ moderator.account }}{% if not loop.last %}, {% endif %}
 {% endfor %}
--- src/weekly.js
+++ src/weekly.js
@@ -9,15 +9,52 @@ const truncate = require('truncate');
 const config = require('../config');
 
 twig.extendFilter("truncate", (value, length) => {
-    return truncate(value, length);
+  return truncate(value, length);
 });
 
 // API endpoints
 const projectsUrl = config.apiUrl + '/api/posts/top?limit=3&start_date=' + moment().add(-7, 'days').format('YYYY-MM-DD') + '&end_date=' + moment().format('YYYY-MM-DD');
 const newcomersUrl = config.apiUrl + '/api/posts/top?limit=4&start_date=' + moment().add(-7, 'days').format('YYYY-MM-DD') + '&end_date=' + moment().format('YYYY-MM-DD') + '&only_new=true';
-const contributionsUrl = config.apiUrl + '/api/posts/top?limit=10&start_date=' + moment().add(-7, 'days').format('YYYY-MM-DD') + '&end_date=' + moment().format('YYYY-MM-DD') + '&retrieve_by=contributions';
+const contributionsUrl = config.apiUrl + '/api/posts/top?limit=10&start_date=' + moment().add(-7, 'days').format('YYYY-MM-DD') + '&end_date=' + moment().format('YYYY-MM-DD') + '&retrieve_by=contributions&sort_by=rewards';
+const moderatorUrl = config.apiUrl + '/api/moderators';
+const sponsorUrl = config.apiUrl + '/api/sponsors';
 
-// get utopian projects/contributions
+let p1 = new Promise((yes, no) => {
+  request(projectsUrl, (err, response, body) => {
+    if (err) no(err);
+    yes(body);
+  });
+})
+
+let p2 = new Promise((yes, no) => {
+  request(newcomersUrl, (err, response, body) => {
+    if (err) no(err);
+    yes(body);
+  });
+});
+
+let p3 = new Promise((yes, no) => {
+  request(contributionsUrl, (err, response, body) => {
+    if (err) no(err);
+    yes(body);
+  });
+});
+
+let p4 = new Promise((yes, no) => {
+  request(moderatorUrl, (err, response, body) => {
+    if (err) no(err);
+    yes(body);
+  });
+});
+
+let p5 = new Promise((yes, no) => {
+  request(sponsorUrl, (err, response, body) => {
+    if (err) no(err);
+    yes(body);
+  });
+});
+
+
 let data = {
   numbering: moment().week() + '/' + moment().year(),
   projects: null,
@@ -27,31 +64,15 @@ let data = {
   sponsors: null
 };
 
-let getData = new Promise((yes, no) => {
-  request(projectsUrl, (err, response, body) => {
-    if (err) no(err);
-    yes(body);
-  });
-});
+Promise.all([p1, p2, p3, p4, p5])
+.then(values => {
+  data.projects = JSON.parse(values[0]);
+  data.newcomers = JSON.parse(values[1]);
+  data.contributions = JSON.parse(values[2]);
+  data.moderators = JSON.parse(values[3]).results;
+  data.sponsors = JSON.parse(values[4]).results;
 
-getData.then((projects) => {
-  data.projects = JSON.parse(projects)
-  request(newcomersUrl, (err, response, body) => {
-    if (err) console.log(err);
-    return body;
-  });
-});
-
-getData.then((newcomers) => {
-  data.newcomers = JSON.parse(newcomers)
-  request(contributionsUrl, (err, response, body) => {
-    if (err) console.log(err);
-    return body;
-  });
-});
-
-getData.then((contributions) => {
-  data.contributions = JSON.parse(contributions)
+  console.log(data.contributions);
 
   // create reusable transporter object using the default SMTP transport
   let mailer = nodemailer.createTransport({
@@ -64,70 +85,64 @@ getData.then((contributions) => {
     }
   });
 
-  getModerators().then((moderators) => {
-    data.moderators = moderators;
-    getSponsors().then((sponsors) => {
-      data.sponsors = sponsors;
+  // read html template
+  twig.renderFile('./src/template.html', {data}, (err, template) => {
+    if (err) {
+      throw err;
+    }
 
-      // read html template
-      twig.renderFile('./src/template.html', {data}, (err, template) => {
-        if (err) {
-          throw err;
+    // save template
+    saveTemplate(template, 'html');
+
+    if (!config.generateOnly) {
+      // mail setup
+      const mailOptions = {
+        from: config.mail.from,
+        to: config.mail.testRecipient,
+        subject: 'Utopian Weekly',
+        text: 'Utopian Weekly',
+        html: template
+      };
+
+      // send mail
+      mailer.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          return console.log(error);
         }
-
-        // save template
-        saveTemplate(template, 'html');
-
-        if (!config.generateOnly) {
-          // mail setup
-          const mailOptions = {
-            from: config.mail.from,
-            to: config.mail.testRecipient,
-            subject: 'Utopian Weekly',
-            text: 'Utopian Weekly',
-            html: template
-          };
-
-          // send mail
-          mailer.sendMail(mailOptions, (error, info) => {
-            if (error) {
-              return console.log(error);
-            }
-            console.log('Message sent: %s', info.messageId);
-          });
-        }
+        console.log('Message sent: %s', info.messageId);
       });
-
-      // read markdown template
-      twig.renderFile('./src/template.md', {data}, (err, template) => {
-        if (err) {
-          throw err;
-        }
-
-        // save template
-        saveTemplate(template, 'md');
-
-        // post on steemit.com
-        if (!config.generateOnly) {
-          let permlink = 'utopian-weekly-' + data.numbering.replace('/', '-');
-
-          steem.broadcast.comment(
-            config.wif,
-            '', // Parent Author
-            '', // Parent Permlink
-            'guest123', // Author
-            permlink, // Permlink
-            'Utopian Weekly - ' + data.numbering, // Title
-            template, // Body,
-            {tags: ['test'], app: 'steemjs/utopianweekly'}, // Json Metadata
-            (err, result) => {
-              console.log(err, result);
-            }
-          );
-        }
-      });
-    });
+    }
   });
+
+  // read markdown template
+  twig.renderFile('./src/template.md', {data}, (err, template) => {
+    if (err) {
+      throw err;
+    }
+
+    // save template
+    saveTemplate(template, 'md');
+
+    // post on steemit.com
+    if (!config.generateOnly) {
+      let permlink = 'utopian-weekly-' + data.numbering.replace('/', '-');
+
+      // steem.broadcast.comment(
+      //   config.wif,
+      //   '', // Parent Author
+      //   '', // Parent Permlink
+      //   'guest123', // Author
+      //   permlink, // Permlink
+      //   'Utopian Weekly - ' + data.numbering, // Title
+      //   template, // Body,
+      //   {tags: ['test'], app: 'steemjs/utopianweekly'}, // Json Metadata
+      //   (err, result) => {
+      //     console.log(err, result);
+      //   }
+      // );
+    }
+  });
+
 });
 
 // fill template with data and save static file
@@ -136,38 +151,8 @@ function saveTemplate(template, ext) {
   let filename = './static/archive/utopian-weekly-' + moment().add(-7, 'days').format('YYYY-MM-DD') + '-' + moment().format('YYYY-MM-DD') + '.' + ext;
   fs.writeFile(filename, template, function(err) {
     if (err) {
-        return console.log(err);
+      return console.log(err);
     }
     console.log('Template saved! (' + filename + ')');
   });
-}
-
-function getModerators() {
-    return new Promise((yes, no) => {
-        request(config.apiUrl + '/api/moderators', (err, res, body) => {
-            if (err) no(err);
-            yes(JSON.parse(body));
-        })
-    }).then((response) => {
-        let mods = [];
-        response.results.forEach((mod) => {
-            mods.push(mod.account);
-        });
-        return mods;
-    })
-}
-
-function getSponsors() {
-    return new Promise((yes, no) => {
-        request(config.apiUrl + '/api/sponsors', (err, res, body) => {
-            if (err) no(err);
-            yes(JSON.parse(body));
-        })
-    }).then((response) => {
-        let sponsors = [];
-        response.results.forEach((sponsor) => {
-            sponsors.push(sponsor.account);
-        });
-        return sponsors;
-    })
 }
