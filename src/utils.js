ac23815fe39b54f551b7453789d57837d9096ac2
mktcode <kontakt@markus-kottlaender.de>
2018-01-16 21:06:32 +0100

    improved code

---
+++ src/utils.js
@@ -0,0 +1,83 @@
+const nodemailer = require('nodemailer');
+const fs = require('fs');
+const steem = require('steem');
+const twig = require('twig');
+const moment = require('moment');
+const truncate = require('truncate');
+const config = require('../config');
+
+const utils = {}
+
+// Twig truncate filter
+twig.extendFilter("truncate", (value, length) => {
+  return truncate(value, length);
+});
+
+utils.generateTemplate = (data, ext) => {
+  return new Promise((resolve, reject) => {
+    twig.renderFile('./src/template.' + ext, {data}, (err, template) => {
+      if (err) {
+        reject(err)
+      }
+
+      resolve(template)
+    });
+  })
+}
+
+utils.saveTemplate = (template, ext) => {
+  // save file
+  let filename = './static/archive/utopian-weekly-' + moment().add(-7, 'days').format('YYYY-MM-DD') + '-' + moment().format('YYYY-MM-DD') + '.' + ext;
+  fs.writeFile(filename, template, function(err) {
+    if (err) {
+      console.log(err);
+    }
+    console.log('Template saved! (' + filename + ')');
+  });
+}
+
+utils.publishWeekly = (html, markdown, subject, permlink) => {
+  // create default SMTP transporter
+  let mailer = nodemailer.createTransport({
+    host: config.smtp.host,
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: config.smtp.user,
+      pass: config.smtp.pass
+    }
+  });
+
+  // mail setup
+  const mailOptions = {
+    from: config.mail.from,
+    to: config.mail.testRecipient,
+    subject: subject,
+    html: html
+  }
+
+  // send mail
+  mailer.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+  })
+
+  // post on steem
+  // steem.broadcast.comment(
+  //   config.wif,
+  //   '', // Parent Author
+  //   '', // Parent Permlink
+  //   'guest123', // Author
+  //   permlink, // Permlink
+  //   subject, // Title
+  //   markdown, // Body,
+  //   {tags: ['test'], app: 'steemjs/utopianweekly'}, // Json Metadata
+  //   (err, result) => {
+  //     console.log(err, result);
+  //   }
+  // );
+}
+
+module.exports = utils
--- src/weekly.js
+++ src/weekly.js
@@ -1,60 +1,30 @@
 'use strict';
 const nodemailer = require('nodemailer');
 const request = require('request');
+const args = require('command-line-args');
 const fs = require('fs');
+const utils = require('./utils');
 const steem = require('steem');
 const twig = require('twig');
 const moment = require('moment');
+const utopian = require("utopian-api");
 const truncate = require('truncate');
 const config = require('../config');
 
-twig.extendFilter("truncate", (value, length) => {
-  return truncate(value, length);
-});
+// Command args
+const defaults = [
+  { name: 'generate', type: Boolean, defaultValue: false },
+  { name: 'generate-and-send', type: Boolean, defaultValue: false },
+  { name: 'send-generated', type: String, defaultValue: false }
+];
+const options = args(defaults);
 
 // API endpoints
 const projectsUrl = config.apiUrl + '/api/posts/top?limit=3&start_date=' + moment().add(-7, 'days').format('YYYY-MM-DD') + '&end_date=' + moment().format('YYYY-MM-DD');
 const newcomersUrl = config.apiUrl + '/api/posts/top?limit=4&start_date=' + moment().add(-7, 'days').format('YYYY-MM-DD') + '&end_date=' + moment().format('YYYY-MM-DD') + '&only_new=true';
 const contributionsUrl = config.apiUrl + '/api/posts/top?limit=10&start_date=' + moment().add(-7, 'days').format('YYYY-MM-DD') + '&end_date=' + moment().format('YYYY-MM-DD') + '&retrieve_by=contributions&sort_by=rewards';
-const moderatorUrl = config.apiUrl + '/api/moderators';
-const sponsorUrl = config.apiUrl + '/api/sponsors';
 
-let p1 = new Promise((yes, no) => {
-  request(projectsUrl, (err, response, body) => {
-    if (err) no(err);
-    yes(body);
-  });
-})
-
-let p2 = new Promise((yes, no) => {
-  request(newcomersUrl, (err, response, body) => {
-    if (err) no(err);
-    yes(body);
-  });
-});
-
-let p3 = new Promise((yes, no) => {
-  request(contributionsUrl, (err, response, body) => {
-    if (err) no(err);
-    yes(body);
-  });
-});
-
-let p4 = new Promise((yes, no) => {
-  request(moderatorUrl, (err, response, body) => {
-    if (err) no(err);
-    yes(body);
-  });
-});
-
-let p5 = new Promise((yes, no) => {
-  request(sponsorUrl, (err, response, body) => {
-    if (err) no(err);
-    yes(body);
-  });
-});
-
-
+// Data
 let data = {
   numbering: moment().week() + '/' + moment().year(),
   projects: null,
@@ -64,93 +34,92 @@ let data = {
   sponsors: null
 };
 
-Promise.all([p1, p2, p3, p4, p5])
-.then(values => {
-  data.projects = JSON.parse(values[0]);
-  data.newcomers = JSON.parse(values[1]);
-  data.contributions = JSON.parse(values[2]);
-  data.moderators = JSON.parse(values[3]).results;
-  data.sponsors = JSON.parse(values[4]).results;
-
-  // create reusable transporter object using the default SMTP transport
-  let mailer = nodemailer.createTransport({
-    host: config.smtp.host,
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: config.smtp.user,
-      pass: config.smtp.pass
-    }
+// Data promises
+let getProjects = new Promise((yes, no) => {
+  request(projectsUrl, (err, response, body) => {
+    if (err) no(err);
+    yes(body);
   });
+})
 
-  // read html template
-  twig.renderFile('./src/template.html', {data}, (err, template) => {
-    if (err) {
-      throw err;
-    }
-
-    // save template
-    saveTemplate(template, 'html');
-
-    if (!config.generateOnly) {
-      // mail setup
-      const mailOptions = {
-        from: config.mail.from,
-        to: config.mail.testRecipient,
-        subject: 'Utopian Weekly',
-        text: 'Utopian Weekly',
-        html: template
-      };
-
-      // send mail
-      mailer.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-      });
-    }
+let getNewcomers = new Promise((yes, no) => {
+  request(newcomersUrl, (err, response, body) => {
+    if (err) no(err);
+    yes(body);
   });
-
-  // read markdown template
-  twig.renderFile('./src/template.md', {data}, (err, template) => {
-    if (err) {
-      throw err;
-    }
-
-    // save template
-    saveTemplate(template, 'md');
-
-    // post on steemit.com
-    if (!config.generateOnly) {
-      let permlink = 'utopian-weekly-' + data.numbering.replace('/', '-');
-
-      // steem.broadcast.comment(
-      //   config.wif,
-      //   '', // Parent Author
-      //   '', // Parent Permlink
-      //   'guest123', // Author
-      //   permlink, // Permlink
-      //   'Utopian Weekly - ' + data.numbering, // Title
-      //   template, // Body,
-      //   {tags: ['test'], app: 'steemjs/utopianweekly'}, // Json Metadata
-      //   (err, result) => {
-      //     console.log(err, result);
-      //   }
-      // );
-    }
-  });
-
 });
 
-// fill template with data and save static file
-function saveTemplate(template, ext) {
-  // save file
-  let filename = './static/archive/utopian-weekly-' + moment().add(-7, 'days').format('YYYY-MM-DD') + '-' + moment().format('YYYY-MM-DD') + '.' + ext;
-  fs.writeFile(filename, template, function(err) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log('Template saved! (' + filename + ')');
+let getContributions = new Promise((yes, no) => {
+  request(contributionsUrl, (err, response, body) => {
+    if (err) no(err);
+    yes(body);
   });
+});
+
+let readGeneratedHtml = new Promise((resolve, reject) => {
+  fs.readFile(options['send-generated'] + '.html', (err, template) => {
+    if (err) {
+      reject(err)
+    }
+    resolve(template)
+  })
+})
+
+let readGeneratedMarkdown = new Promise((resolve, reject) => {
+  fs.readFile(options['send-generated'] + '.md', (err, template) => {
+    if (err) {
+      reject(err)
+    }
+    resolve(template)
+  })
+})
+
+// Generate (and send)
+if (options['generate'] || options['generate-and-send']) {
+  Promise.all([
+    // load data
+    getProjects,
+    getNewcomers,
+    getContributions,
+    utopian.getModerators(),
+    utopian.getSponsors()
+  ])
+    .then(values => {
+      data.projects = JSON.parse(values[0]);
+      data.newcomers = JSON.parse(values[1]);
+      data.contributions = JSON.parse(values[2]);
+      data.moderators = values[3].results;
+      data.sponsors = values[4].results;
+
+
+      Promise.all([
+        // generate templates with data
+        utils.generateTemplate(data, 'html'),
+        utils.generateTemplate(data, 'md'),
+      ]).then((templates) =>  {
+        // save templates to archive
+        utils.saveTemplate(templates[0], 'html');
+        utils.saveTemplate(templates[1], 'md');
+
+        if (options['generate-and-send']) {
+          utils.publishWeekly(templates[0], templates[1], 'Utopian Weekly - ' + data.numbering, 'utopian-weekly-' + data.numbering.replace('/', '-'))
+        }
+      }).catch((err) => {
+        console.log(err);
+      })
+    }).catch((err) => {
+      console.log(err);
+    });
+}
+
+// Send previouisly generated
+if (options['send-generated'] && !options['generate'] && !options['generate-and-send']) {
+  Promise.all([
+    readGeneratedHtml,
+    readGeneratedMarkdown
+  ]).then((templates) => {
+    utils.publishWeekly(templates[0], templates[1], 'Utopian Weekly - ' + data.numbering, 'utopian-weekly-' + data.numbering.replace('/', '-'))
+  }).catch((err) => {
+    console.log(err);
+  })
 }
